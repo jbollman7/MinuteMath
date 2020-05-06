f2014241fc010a41ae4a960d2e51164da6371437
jumpman <joseph.bollman@gmail.com>
2020-05-05 22:22:47 -0600

    Fixed icon, change it at the project level, You dont need to add png for each screen px density dir. Just at the VS project level under Android Application. Implemented SPlash screen, updated game logo, and game icon.

--- MinuteMath.Android/MainActivity.cs
+++ MinuteMath.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MinuteMath.Droid
 {
-    [Activity(Label = "MinuteMath", Icon = "@drawable/roundlogo", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MinuteMath", Icon = "@drawable/roundlogo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
